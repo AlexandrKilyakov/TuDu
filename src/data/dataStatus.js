9c2ded6768f8171a22ae1637d190a4025f63db30
Alexander Kilyakov <kalataaleksandr@gmail.com>
2025-06-29 01:23:57 +0300

    Собранный проект

--- src/data/dataStatus.js
+++
@@ -1,9 +0,0 @@
-const dataStatus = {
-  initial: "Начальный",
-  in_progress: "В процессе",
-  paused: "На паузе",
-  completed: "Завершен",
-  canceled: "Отменен",
-};
-
-export default dataStatus;
